194eac3942e787683c35b5a7c21fb752e7ed9b42
Manu <manu@estadologico.com>
2021-10-29 22:23:50 +0200

    Clean files

--- raycast/scripts/sofi-horario.js
+++ raycast/scripts/sofi-horario.js
@@ -9,7 +9,8 @@
 const fs = require('fs')
 const pdf = require('pdf-parse') // https://gitlab.com/autokent/pdf-parse
 
-const FILE_PATH = '/Users/manu/Downloads/hamman.pdf'
+// const FILE_PATH = '/Users/manu/Downloads/hamman.pdf'
+const FILE_PATH = '/Users/manu/Library/Mobile Documents/com~apple~CloudDocs/Horario.pdf'
 let dataBuffer = fs.readFileSync(FILE_PATH)
 
 // Configuración
@@ -31,7 +32,7 @@ const DAY_BREAK   = 'TMMD'
 
 /**
  * TODO
- * [ ] Get and show rol (A, M, AE, ...)
+ * [X] Get and show rol (A, M, AE, ...)
  * [ ] Green color for Free day
  * [ ] Color for tables
  * 
@@ -54,8 +55,8 @@ pdf(dataBuffer).then(function(data) {
     days.forEach((day, dayID) => {
       if(day.includes(PERSON)){
         day.split('\n').forEach((line) => {
-          if(line.includes(PERSON)) {
-            rol = line.substring(1, 2)
+          if(line.includes(PERSON)) {            
+            rol = line.substring(1, line.indexOf(')'))
           }
         })
         dataJSON[dayID].push([turnID, rol])
@@ -63,18 +64,24 @@ pdf(dataBuffer).then(function(data) {
     })
   })
 
+  console.log(dataJSON)
+
   dataJSON.forEach((day, dayID) => {
+    let space = '     '
     let out = ''
     let dayName = DAYS_NAME[dayID] + '     '
     out = dayName.substring(0, 10)
     
     if(day.length) {
-      let rol = day[0][1]
-      let turnStart = day[0][0]
-      let turnEnd   = day[day.length-1][0] + 1
+      let rol = `(${day[0][1]}) `
+      let turnStart = day[0][0] - 1
+      let turnEnd   = day[day.length-1][0]
 
-      out += `(${rol})   ${TURNS[turnStart]}   `
-      out += TURNS[turnEnd]
+      rol = rol.substring(0, 4)
+
+      out += `${space}${TURNS[turnStart]} ${rol}`
+      out += `${space}${TURNS[turnEnd]} ${rol}`
+      out += `${space}${day.length * 2} horas`
     } else {
       out += 'LIBRE'
     }
